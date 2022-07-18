6fadcdab4bc01415a190eb07a2bd5f481ea69463
michaelballos <mdb3091207@gmai.com>
2022-07-18 15:40:45 -0500

    Add mock pages

---
+++ pages/projects/my-projects/overview/dashboard/index.tsx
@@ -0,0 +1,28 @@
+import {
+  Text,
+  Title,
+} from '@mantine/core';
+
+export default function MyProjects() {
+  return (
+    <div
+      style={{
+        display: 'flex',
+        justifyContent: 'center',
+        alignItems: 'center',
+        height: '100%',
+        width: '100%',
+      }}
+    >
+      <Title
+        align="center"
+        mt={100}
+      >
+        Welcome to{' '}
+        <Text inherit variant="gradient" component="span">
+          Dashboard
+        </Text>
+      </Title>
+    </div>
+  );
+}
